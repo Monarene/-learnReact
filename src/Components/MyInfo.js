b3645045e0ac1c3c7ac028253d137f70cdc4ce47
Michael Mekuleyi <mekuleyimichael@gmail.com>
2019-07-06 05:40:37 +0100

    The very first commuit

---
+++ src/Components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo(){
+    return (
+        <div>
+            <h1>Michael Mekuleyi</h1>
+            <p>I like building stuff, technology stuff</p>
+            <ul>
+                <li>United States Of America</li>
+                <li>Germany</li>
+                <li>Canada</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
